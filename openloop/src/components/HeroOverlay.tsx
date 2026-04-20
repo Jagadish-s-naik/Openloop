65d9a6fc248b7e7c8151fc713e29b9aa7e4b3636
dineshyr29-04 <dineshyr2904@gmail.com>
2026-04-20 23:32:58 +0530

    feat: create HeroOverlay component with countdown timer, navigation, and interactive elements

--- openloop/src/components/HeroOverlay.tsx
+++ openloop/src/components/HeroOverlay.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  type TimerMode,
-} from '../utils/timerClient';
 import { useTimerSync } from '../hooks/useTimerSync';
 
 interface HeroOverlayProps {
